73bab6a143ad4ea5d0b4ec4172df105fc39dfa48
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-09-30 10:38:46 +0800

    Videos API

--- app/Http/Controllers/Api/VideosController.php
+++ app/Http/Controllers/Api/VideosController.php
@@ -3,24 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use App\Services\VideosService;
+use App\Http\Requests\Videos\CreateVideoRequest;
+use App\Http\Requests\Videos\UpdateVideoRequest;
 
 class VideosController extends Controller
 {
+    public function __construct(
+        private readonly VideosService $videosService
+    ) {}
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index() : JsonResponse
     {
-        //
+        $result = $this->videosService->getUnpaginated();
+
+        return response()->json($result, Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateVideoRequest $request)
     {
-        //
+        $result = $this->videosService->create($request->validated());
+
+        return response()->json($result, Response::HTTP_CREATED);
     }
 
     /**
@@ -28,15 +39,19 @@ class VideosController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $result = $this->videosService->getById($id);
+
+        return response()->json($result, Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateVideoRequest $request, string $id)
     {
-        //
+        $result = $this->videosService->updateById($id, $request->validated());
+
+        return response()->json($result, Response::HTTP_CREATED);
     }
 
     /**
@@ -44,6 +59,8 @@ class VideosController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $result = $this->videosService->deleteById($id);
+
+        return response()->json($result, Response::HTTP_NO_CONTENT);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\VideosController;
 
 
 Route::prefix('v1')->group(function () {
-   Route::prefix('auth')->group(function () {
-       Route::post('login', [AuthController::class, 'signin']);
-   });
+    // Auth APIs
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'signin']);
+    });
+
+    // Videos APIs
+    Route::apiResource('videos', VideosController::class);
 });
